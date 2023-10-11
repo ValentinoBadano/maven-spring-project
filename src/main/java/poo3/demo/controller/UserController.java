58122ba065af92923a7790099a767ab953220e3d
ValentinoBadano <valentinobadano@gmail.com>
2023-10-11 17:53:15 -0300

    agregada la capacidad de registro

--- src/main/java/poo3/demo/controller/UserController.java
+++ src/main/java/poo3/demo/controller/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import poo3.demo.model.User;
 import poo3.demo.service.UserServiceImp;
 
@@ -18,4 +20,19 @@ public class UserController {
         model.addAttribute("user", new User());
         return "login";
     }
+
+    @GetMapping("/register")
+    public String registrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "registration";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute("user") User user) {
+        // Validar y guardar el usuario en la base de datos
+        userService.create(user);
+        return "redirect:/login";
+    }
+
+
 }
