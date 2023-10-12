cd73bdf7bafd23464407c405cac930902399c67f
ValentinoBadano <valentinobadano@gmail.com>
2023-10-11 21:34:31 -0300

    controlador dividido en login y register

--- src/main/java/poo3/demo/controller/LoginController.java
+++ src/main/java/poo3/demo/controller/LoginController.java
@@ -4,29 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import poo3.demo.model.User;
 import poo3.demo.service.UserServiceImp;
 
 @Controller
-public class UserController {
-
+public class LoginController {
     @Autowired
     private UserServiceImp userService; // Debes tener un servicio para gestionar la autenticación
 
-    @GetMapping("/register")
-    public String registrationForm(Model model) {
+    @GetMapping("/login")
+    public String showLoginForm(Model model) {
         model.addAttribute("user", new User());
-        return "register";
+        return "login";
     }
-
-    @PostMapping("/register")
-    public String registerUser(@ModelAttribute("user") User user) {
-        // Validar y guardar el usuario en la base de datos
-        userService.create(user);
-        return "redirect:/login";
-    }
-
-
 }
