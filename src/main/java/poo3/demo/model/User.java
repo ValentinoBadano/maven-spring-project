bbb62773d6d760592bef6cb4fccace0511eb9178
ValentinoBadano <valentinobadano@gmail.com>
2023-10-27 16:56:50 -0300

    Agregados atributos de User

--- src/main/java/poo3/demo/model/User.java
+++ src/main/java/poo3/demo/model/User.java
@@ -10,6 +10,9 @@ import java.util.Collection;
 public class User implements UserDetails {
     private String username;
     private String password;
+    private String name;
+    private String last_name;
+    private String email;
 
     public String getUsername() {
         return username;
@@ -51,5 +54,29 @@ public class User implements UserDetails {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
