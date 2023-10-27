dc56dc2c6a807d20d93bc3e2f3c0cb55ac36a01e
ValentinoBadano <valentinobadano@gmail.com>
2023-10-27 17:20:10 -0300

    Finalizada la tabla de usuarios

--- src/main/java/poo3/demo/controller/IndexController.java
+++ src/main/java/poo3/demo/controller/IndexController.java
@@ -1,13 +1,20 @@
 package poo3.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import poo3.demo.service.UserServiceImp;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserServiceImp userService;
+
     @GetMapping("/index")
-    public String welcomePage() {
+    public String welcomePage(Model model) {
+        model.addAttribute("users", userService.getUsers());
         return "index";
     }
 }
